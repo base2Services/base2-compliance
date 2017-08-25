6068b845c2b0c7c5948e0078c43fb9e1722b8f16
Aaron Walker <aaronpwalker@gmail.com>
2017-08-25 15:59:37 +1000

    updated impact of sysctl-7 and sysctl-10

--- controls/base2_spec.rb
+++ controls/base2_spec.rb
@@ -5,13 +5,13 @@ title 'base2 compliance'
 
 include_controls 'linux-baseline' do
   control 'os-10' do
-    impact 0.5
+    impact 0
   end
   control 'sysctl-07' do
-    impact 0.5
+    impact 0
   end
   control 'sysctl-10' do
-    impact 0.5
+    impact 0
   end
 
 
